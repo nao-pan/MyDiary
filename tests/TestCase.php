c6762daafb5745919191df6f1d2cfafe38cf7a68
nao-pan <n.hasegawa.su@gmail.com>
2025-08-18 04:46:11 +0000

    viteのエラー解消

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,12 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // テスト時は Vite を無効化（@vite が空になる）
+        $this->withoutVite();
+    }
 }
